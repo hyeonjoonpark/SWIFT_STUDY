7f0a06b62212d924367e69e278039bf6a774601d
hyeonjoonpark <pjjoon1379@gmail.com>
2024-07-10 07:26:14 +0900

    Initial Commit

---
+++ SWIFT_STUDY/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SWIFT_STUDY
+//
+//  Created by 박현준 on 7/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+        .padding()
+    }
+}
+
+struct ContentView_Preview: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SWIFT_STUDY/SWIFT_STUDYApp.swift
@@ -0,0 +1,17 @@
+//
+//  SWIFT_STUDYApp.swift
+//  SWIFT_STUDY
+//
+//  Created by 박현준 on 7/10/24.
+//
+
+import SwiftUI
+
+@main
+struct SWIFT_STUDYApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
